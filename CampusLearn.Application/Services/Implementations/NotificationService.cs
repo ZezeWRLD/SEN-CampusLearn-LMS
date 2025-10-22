e79783471da7a247c8ea5e2a00ada6f2cff3daba
Zandre Beukes <578127@student.belgiumcampus.ac.za>
2025-10-22 17:47:51 +0200

    feat(TwilioAPIs): Add Twilio and SendGrid NuGet packages to solution, added Email, WhatsApp, and SMS logic to the NotificationService class and INotificationService interface. Add placeholders for API's in appsettings.json
    -Added twilio and sendgrid nuget package for use in solution
    -Added whatsapp, email, and sms logic. Only email is implemente, the rest are for future use and to show how integration would look
    -Added placeholder API tokens to appsettings.json

--- CampusLearn.Application/Services/Implementations/NotificationService.cs
+++ CampusLearn.Application/Services/Implementations/NotificationService.cs
@@ -7,17 +7,42 @@ using CampusLearn.Application.Services.Interfaces;
 using CampusLearn.Infrastructure.Data;
 using CampusLearn.Infrastructure.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using Microsoft.Extensions.Configuration;
+
 
 namespace CampusLearn.Application.Services.Implementations
 {
     public class NotificationService : INotificationService
     {
         private readonly CampusLearnDbContext _context;
+        private readonly string _twilioSid;
+        private readonly string _twilioToken;
+        private readonly string _twilioNumber;
+        private readonly string _sendgridKey;
+        private readonly string _fromEmail;
 
-        public NotificationService(CampusLearnDbContext context)
+        public NotificationService(CampusLearnDbContext context, IConfiguration configuration)
         {
             _context = context;
-        }
+            _twilioSid = configuration["Twilio:AccountSid"]
+                ?? throw new InvalidOperationException("Missing Twilio AccountSid");
+            _twilioToken = configuration["Twilio.AuthToken"]
+                ?? throw new InvalidOperationException("Missing Twilio AuthToken");
+            _twilioNumber = configuration["Twilio.FromNumber"]
+                ?? throw new InvalidOperationException("Missing Twilio FromNumber");
+            _sendgridKey = configuration["SendGrid.ApiKey"]
+                ?? throw new InvalidOperationException("Missing SendGrid ApiKey");
+            _fromEmail = configuration["SendGrid.FromEmail"]
+                ?? throw new InvalidOperationException("Missing SendGrid FromEmail");
+
+            TwilioClient.Init(_twilioSid, _twilioToken);
+        } 
+        
         public async Task<IEnumerable<Notification>> GetAllNotificationsAsync() => await _context.Notifications.ToListAsync();
         public async Task<Notification> GetNotificationAsync(int id)
         {
@@ -48,6 +73,47 @@ namespace CampusLearn.Application.Services.Implementations
             _context.Notifications.Remove(notification);
             await _context.SaveChangesAsync();
             return true;
+        }
+        public async Task SendNotificationToUserAsync(int userId, string subject, string message)
+        {
+            var preference = await _context.Notificationpreferences
+                .FindAsync(userId);
+            if (preference == null) 
+                throw new InvalidOperationException($"No notification preferences found for user {userId}");
+            var user = preference.User;
+            if (preference.EmailEnabled == true)
+            {
+                await SendEmailAsync(user.UserEmail, subject, message);
+            }
+            else
+            {
+                await SendEmailAsync (user.UserEmail, subject, message);
+            }
+        }
+        public async Task SendSmsAsync(string to, string message) //unused. Just here for future expansion and to show future implementation
+        {
+            await MessageResource.CreateAsync(
+                body: message,
+                from: new PhoneNumber(_twilioNumber),
+                to: new PhoneNumber(to)
+                );
+        }
+        public async Task SendWhatsappAsync(string to, string message) //unused. Just here for future expansion and to show future implementation
+        {
+            await MessageResource.CreateAsync(
+                body: message,
+                from: new PhoneNumber("whatsapp:+14155238886"),
+                to: new PhoneNumber($"whatsapp:{to}")
+                );
+        }
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            SendGridClient client = new SendGridClient(_sendgridKey);
+            EmailAddress emailAddress = new EmailAddress(_fromEmail, "CampusLearn");
+            string htmlBody = $"<p>{body}</p>";
+            SendGridMessage msg = MailHelper.CreateSingleEmail(emailAddress, new EmailAddress(to), subject, body, htmlBody);
+            Response response = await client.SendEmailAsync(msg);
+
         }
 
     }
